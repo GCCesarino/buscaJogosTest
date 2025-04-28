73816a7e47d6549a987aadb3cd07062883a37d17
Gustavo Cesarino <gccesarino220@gmail.com>
2025-04-27 21:36:29 -0300

    sera que vai?

--- src/main.ts
+++ src/main.ts
@@ -3,7 +3,7 @@ import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  app.enableCors();
-  await app.listen(3000);
+  app.enableCors(); // 👈 Habilite CORS para o frontend se comunicar
+  await app.listen(process.env.PORT || 3000); // 👈 Usa a porta do Vercel ou 3000 local
 }
 bootstrap();
